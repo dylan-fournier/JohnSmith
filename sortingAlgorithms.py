26507c6efd54fe83c68636e57aa1c59ca5a8c27f
dylan-fournier <dylan.fournier@stu.ocsb.ca>
2023-04-21 11:14:06 -0400

    commit more like omit am i right gamers

--- sortingAlgorithms.py
+++ sortingAlgorithms.py
@@ -25,11 +25,11 @@ def bubble(myList):
                     temp = myList[i]
                     myList[i] = myList[i-1]
                     myList[i-1] = temp
-                if i == len(myList):
-                    return myList
+        return myList
+
 def bubbleSort(myListStatic,numofloops=1):
     timefinal = 0
-    myList =myListStatic
+    myList=myListStatic.copy()
     for x in range(numofloops):
         time1 = time.time()
         myList=myListStatic.copy()
@@ -51,3 +51,29 @@ def bogoSort(myListStatic,numofloops=1):
         time2 = time.time()
         timefinal = timefinal + (time2-time1)
     return(timefinal/numofloops)
+
+def insertionSort(myListStatic, numofloops=1):
+    timefinal = 0
+    myList = myListStatic.copy()
+    for i in range(numofloops):
+        time1 = time.time()
+        myList=myListStatic.copy()
+        while orderChecker(myList) ==False:
+            myList=insertion(myList)
+        time2 =time.time()
+        timefinal= timefinal+ (time2-time1)
+    return(timefinal/numofloops)
+
+def insertion(myList):
+    for i in range(len(myList)):
+            if i == 0:
+                pass
+            else:
+                if myList[i] >= myList[i-1]:
+                    pass
+                else:
+                    for j in range(i):
+                        temp = myList[i-j]
+                        myList[i-j] = myList[i-j-1]
+                        myList[i-j-1] = temp
+    return myList
--- sortingTest.py
+++ sortingTest.py
@@ -1,4 +1,5 @@
 import sortingAlgorithms as alg
-array = [4,3,2,1,0]
+array = [10,9,8,7,5,111,65,6,4,3,2,1,0]
 print("bogosort: " + str(alg.bogoSort(array)))
 print("bubblesort: "+ str(alg.bubbleSort(array)))
+print("insertion: "+str(alg.insertionSort(array)))
