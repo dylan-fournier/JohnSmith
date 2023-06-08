8916682470c39ee808ac3d85845c0eddf580bcf7
dylan-fournier <dylan.fournier@stu.ocsb.ca>
2023-06-08 00:23:54 -0400

    barcode database scanner

---
+++ barcodedatabase.py
@@ -0,0 +1,66 @@
+import pickle
+
+class item():
+    def __init__(self,name,price):
+        self.name = name
+        self.price = float(price)
+    def __str__(self):
+        return f"name:{self.name} | Price:{self.price} "
+try:
+    with open('database of barcodes.pkl', 'rb') as f:
+        database = pickle.load(f)
+except:
+    database = {
+
+    }  
+
+mainLoop = True
+
+while mainLoop:
+    print("0. Exit \n1. Add Code \n2. Print List of Codes\n3. Checkout \n4. Remove Code")
+    firstIn = int(input("? "))
+    if firstIn == 0:
+        mainLoop = False
+    elif firstIn == 1:
+        tempbarcode = input("Scan the Barcode now\n? ")
+        print("Name of this item?")
+        tempname = input("? ")
+        print("price of this item")
+        tempprice = float(input("? "))
+        try:
+            database[tempbarcode]
+            print("this code already exists, overwrite?")
+            x = input("y/n ? ")
+            if x == "y":
+                database[tempbarcode] = item(tempname,tempprice)
+            else:
+                pass
+        except:
+            database[tempbarcode] = item(tempname,tempprice)
+    elif firstIn == 2:
+        print()
+        for i in database:
+            print(database[i])
+        print()
+    elif firstIn == 3:
+        temptotal = 0
+        scanLoop = True
+        while scanLoop:
+            barcodeIn = input("? ")
+            if barcodeIn == str(0):
+                scanLoop = False
+            else:
+                try:
+                    temptotal = temptotal + database[barcodeIn].price
+                except:
+                    print("item does not exist")
+            print("Total Price: $"+str(temptotal))
+    elif firstIn == 4:
+        x = input("Barcode? ")
+        try:
+            print(database[x].name + " Removed")
+            database.pop(x)
+        except:
+            print("Not in database yet")
+with open('database of barcodes.pkl', 'wb') as f:
+    pickle.dump(database, f)
